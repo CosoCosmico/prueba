efbbfe3221b5b91015008e3a84c742652f1dce29
CosoCosmico <javierpavez508@gmail.com>
2022-09-14 10:18:37 -0300

    se codifico ts y html de formulario-seccion y se modifico un detalle del html de formulario-alumno

--- src/app/componentes/formularios/formulario-seccion/formulario-seccion.component.ts
+++ src/app/componentes/formularios/formulario-seccion/formulario-seccion.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { seccion } from '../../modelos/seccion';
 
 @Component({
   selector: 'app-formulario-seccion',
   templateUrl: './formulario-seccion.component.html',
   styleUrls: ['./formulario-seccion.component.css']
 })
-export class FormularioSeccionComponent implements OnInit {
+export class FormularioSeccionComponent {
+@Output() public seccionFormulario = new EventEmitter<seccion>();
 
-  constructor() { }
+ public nuevaSeccion: seccion = {
+  id:0,
+  seccion:''
+ }
 
-  ngOnInit(): void {
-  }
+ public seccionId (evento: Event): void{
+  const variable = evento.target as HTMLInputElement;
+  this.nuevaSeccion.id = Number.parseInt(variable.value);
+ }
+
+ public seccionDocente (evento: Event): void{
+  const elemento = evento.target as HTMLInputElement;
+  this.nuevaSeccion.seccion = elemento.value;
+ }
+
+ public guardarSeccion():void{
+  const copiaSeccion: seccion = {
+    ...this.nuevaSeccion
+  };
+  this.seccionFormulario.emit(copiaSeccion);
+  this.nuevaSeccion.id = 0;
+  this.nuevaSeccion.seccion = '';
+}
 
 }
