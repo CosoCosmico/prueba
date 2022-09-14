33c9fedb37a8f099784cdc483e2c21cb79a51ce6
CosoCosmico <javierpavez508@gmail.com>
2022-09-14 10:45:30 -0300

    se codifico ts y html(falta por completar) en el componente inicial de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { alumno } from './componentes/modelos/alunmno';
+import { seccion } from './componentes/modelos/seccion';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prueba';
+ public nuevoAlumno: Array<alumno> = [{
+  rut:11111111-1,
+  nombre:'',
+  apellido:'',
+  edad:17
+ }];
+
+ public guardarElemento(alumnos: alumno): void{
+  const id = this.nuevoAlumno.length + 1;
+  alumnos.rut = id;
+  this.nuevoAlumno.push(alumnos);
+ }
+
+ public nuevaSeccion: Array<seccion> = [{
+ id:0,
+ seccion:''
+ }];
+
+ public guardarElementoSeccion(secciones: seccion): void{
+ const id = this.nuevaSeccion.length + 1;
+ secciones.id = id;
+ this.nuevaSeccion.push(secciones);
+ }
 }
